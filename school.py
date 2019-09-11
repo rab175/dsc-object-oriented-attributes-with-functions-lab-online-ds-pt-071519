a06f70103370ea47826bee929cbbf89c734409da
Ryan Beck <ryanbeck175@gmail.com>
2019-09-10 22:02:55 -0400

    complete

--- school.py
+++ school.py
@@ -1,1 +1,24 @@
 class School:
+    
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+        
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+            
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        new_dict = self.roster
+        for key in new_dict:
+            new_dict[key].sort()
+        return new_dict
+
+
+
+
